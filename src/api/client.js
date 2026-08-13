32a478cf823f9a49572b09122e6968ab9eddef75
Sohel Shaikh <sohel.techin@gmail.com>
2026-08-13 23:56:35 +0530

    Refactor: Changed Backend URL for deploy

--- src/api/client.js
+++ src/api/client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
     // baseURL: import.meta.env.VITE_API_RENDER_URL,
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_RENDER_URL,
     withCredentials: true
 })
 
